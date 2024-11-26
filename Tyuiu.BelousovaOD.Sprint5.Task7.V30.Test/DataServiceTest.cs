7179c799d56cebf59e150f1d9a1a4d4927f94306
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2024-11-27 03:53:07 +0500

    Task7

---
+++ Tyuiu.BelousovaOD.Sprint5.Task7.V30.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BelousovaOD.Sprint5.Task7.V30.Lib;
+namespace Tyuiu.BelousovaOD.Sprint5.Task7.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckFileExists()
+        {
+            string path = @"C:\Users\79519\source\repos\Tyuiu.BelousovaOD.Sprint5\DataSprint5\InPutDataFileTask7V30.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.BelousovaOD.Sprint5.Task7.V30/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.BelousovaOD.Sprint5.Task7.V30.Lib;
+namespace Tyuiu.BelousovaOD.Sprint5.Task7.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Белоусова О. Д. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнила: Белоусова Ольга Дмитриевна |  СМАРТб-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Заменить все однозначные числа на число \"9*\". Полученный результат    *");
+            Console.WriteLine("* сохранить в файл формата txt.                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+
+            string path = Path.Combine(@"C:\Users\79519\source\repos\Tyuiu.BelousovaOD.Sprint5\DataSprint5", "InPutDataFileTask7V30.txt");
+            Console.WriteLine($"Данные находятся в файле: {path}");
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Находится в файле:");
+            Console.WriteLine(ds.LoadDataAndSave(path));
+            Console.ReadKey();
+        }
+    }
+}
